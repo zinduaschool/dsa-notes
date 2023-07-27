2e2167f82c147bd10be1171b86afa09b5204ae86
Cyril Michino <cyrilmichino@gmail.com>
2023-07-27 11:49:02 +0300

    House robber solution

--- DynamicProgramming/houserobber/houserobber.py
+++ DynamicProgramming/houserobber/houserobber.py
@@ -1,8 +1,16 @@
 def houserobber(lst):
+    if len(lst) == 0:
+        return None
+    a = lst[0]
     if len(lst) == 1:
-        return lst[0]
+        return a
+    b = max(lst[0], lst[1])
     if len(lst) == 2:
-        if lst[0] > lst[1]:
-            return lst[0]
-        return lst[1]
-    
+        return b
+    for i in range(2,len(lst)):
+        c = max(b,a+lst[i])
+        a = b
+        b = c
+    return c
+
+print(houserobber([2,1,2,1,1,2]))
