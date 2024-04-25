9b3baa941b1e22ee5bf48e6949f32c8e09210ec4
Cyril Michino <cyrilmichino@gmail.com>
2024-04-25 12:23:25 +0300

    0-1 Knapsack top-down

--- content/DynamicProgramming/L2-0-1-knapsack/knapsack.py
+++ content/DynamicProgramming/L2-0-1-knapsack/knapsack.py
@@ -1,5 +1,23 @@
-maxW = 8
-w = [1,3,5,7]
-p = [2,4,7,10]
+cache = dict() #(w,maxW) | maxP
+def knapsack(maxW,w,p):
+    if len(w) == 0:
+        return 0
+    if len(w) == 1:
+        if w[0] <= maxW:
+            return p[0]
+        return 0
+    if (tuple(w),maxW) in cache.keys():
+        return cache[(tuple(w),maxW)]
+    if w[-1] <= maxW:
+        a = p[-1] + knapsack(maxW-w[-1],w[:-1],p[:-1])
+        b = knapsack(maxW,w[:-1],p[:-1])
+        cache[(tuple(w),maxW)] = max(a,b)
+        return cache[(tuple(w),maxW)]
+    cache[(tuple(w),maxW)] = knapsack(maxW,w[:-1],p[:-1])
+    return cache[(tuple(w),maxW)]
 
-def knapsack():
+
+maxW = 8
+w = [3,5,7]
+p = [4,7,10]
+print(knapsack(maxW,w,p))
